e096b9c41260c977f1ded1673a9216b62198c18d
Admin <ainemrty1@gmail.com>
2025-04-01 15:32:55 +0700

    init commit

--- src/main/java/com/KLTN/nguyen/hotelbooking/entity/Room.java
+++ src/main/java/com/KLTN/nguyen/hotelbooking/entity/Room.java
@@ -14,6 +14,8 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(columnDefinition = "NVARCHAR(MAX)")
+    private String roomName;
 
     private Double price;
 
